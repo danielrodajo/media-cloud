dae056eec503b6caa300073658ab479c29d1b314
Daniel <daniel.rodajo98@gmail.com>
2020-04-15 13:42:41 +0200

    -

--- src/components/BodyList/BodyList.jsx
+++ src/components/BodyList/BodyList.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import MenuList from './MenuList/MenuList'
 import FileList from './FileList/FileList'
 
-const files = ['prueba.png', 'palermo.jpg', 'text.txt', 'text2.txt', 'text3.txt', 'text4.txt', 'text5.txt', 'text6.txt', , 'text7.txt', , 'text8.txt', , 'text9.txt']
+const files = ['prueba.png', 'palermo.jpg', 'text.txt', 'text2.txt', 'text3.txt', 'text4.txt', 'text5.txt', 'text6.txt', 'text7.txt', 'text8.txt' , 'text9.txt']
 
 const BodyList = () => {
     return (
         <div>
-            <MenuList text="Inicio"/>
+            <MenuList text="Archivos Recientes"/>
             <FileList files={files}/>
         </div>
     );
