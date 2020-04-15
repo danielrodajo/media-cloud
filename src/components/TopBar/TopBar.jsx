27ea3a84ae4ec0e30d302c07e20222a095bc304d
Daniel <daniel.rodajo98@gmail.com>
2020-04-15 12:27:22 +0200

    Primer componente - TopBar

---
+++ src/components/TopBar/TopBar.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const TopBar = props => {
+    return (
+        <div>
+            <h3>{props.title}</h3>
+        </div>
+    );
+}
+ 
+export default TopBar;
