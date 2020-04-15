35d1939d3543ccf96a4251cc8aaf86d0cb96535b
Daniel <daniel.rodajo98@gmail.com>
2020-04-15 12:17:18 +0200

    Parentesis para Raul

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
 
 const rootReducer = combineReducers({
 });
