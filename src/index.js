9ed55b56cbd148987f5040f9f4316aa14cd36fef
Daniel <daniel.rodajo98@gmail.com>
2020-04-16 10:36:46 +0200

    Store extraido a un fichero propio

--- src/index.js
+++ src/index.js
@@ -2,20 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import generateStore from './store/store'
 
-const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
-
-const rootReducer = combineReducers({
-});
-
-const store = createStore(rootReducer, composeEnhancers(
-  applyMiddleware(thunk)
-));
+const store = generateStore();
 
 ReactDOM.render(
     <Provider store={store}>
---
+++ src/store/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({
+
+});
+
+//Configurar Store con el Plugin de Redux DevTools de Google Chrome
+const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
+
+
+export default function generateStore() {
+    const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+
+    return store;
+}
