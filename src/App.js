869cbf62650fe2b2cb34fce6dcfcfb48e05a82a4
Daniel <daniel.rodajo98@gmail.com>
2020-04-17 10:41:32 +0200

    Agregados hosting y sistema de autenticación con AWS Amplify.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 import TopBar from './components/TopBar/TopBar';
 import BottomBar from './components/BottomBar/BottomBar';
 import ListButtons from './components/BottomBar/ListButtons';
-import BodyList from './components/BodyList/BodyList';
 import { Switch, Route } from 'react-router-dom';
 import FileRetriever from './containers/FileRetriever/FileRetriever';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator, Authenticator } from 'aws-amplify-react';
+import '@aws-amplify/ui/dist/style.css';
+
+Amplify.configure(awsconfig);
 
 function App() {
 
@@ -20,10 +25,11 @@ function App() {
   return (
     <Fragment>
       <TopBar title="Inicio"/>
+      <Authenticator usernameAttributes="email"/>
       <Suspense fallback={<p>Loading...</p>}>{routes}</Suspense>
       <BottomBar buttons = {ListButtons()}/>
     </Fragment>  
   );
 }
 
-export default App;
+export default withAuthenticator(App, { usernameAttributes: 'email' });
