1a84af7c5f2be40dc8eae1e77d69e2a3d5dbf670
raul <cadiernoraul@gmail.com>
2020-09-24 03:22:49 +0200

    añadido la foto de usuario, el nombre del usuario y el boton de confi…

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { IonPage, IonContent, IonText } from '@ionic/react';
+import { IonPage, IonContent, IonText, IonAvatar, IonLabel, IonImg, IonButton } from '@ionic/react';
 import Toolbar from '../../components/ToolBar/Toolbar';
-import { CircularProgressbar } from 'react-circular-progressbar';
+import userdefault from "../../images/unnamed.jpg";
 import 'react-circular-progressbar/dist/styles.css';
 import './Profile.css';
-import AnimatedCheckmark from '../../components/AnimatedCheckmark/AnimatedCheckmark';
 
 const Profile: React.FC = () => (
     <IonPage>
         <Toolbar />
         <IonContent>
+            <IonAvatar className="avatar">
+                <IonImg src={userdefault} alt="user"/>
+            </IonAvatar>
+            <IonLabel className="namelabel">User Name</IonLabel>
+            <IonButton className="botton">Configuracion</IonButton>            
         </IonContent>
     </IonPage>
 )
