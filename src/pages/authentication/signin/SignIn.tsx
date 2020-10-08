29a87897f028bb20d0941c5cdf9b187d92c9cc3b
raul <cadiernoraul@gmail.com>
2020-10-09 00:57:43 +0200

    controlados los errores emitidos en la pestaña de login, tambien el boton de iniciar sesion permanecera deshabilitado hasta que los inputs tengan texto

--- src/pages/authentication/signin/SignIn.tsx
+++ src/pages/authentication/signin/SignIn.tsx
@@ -24,7 +24,38 @@ const SignIn: React.FC<props> = props => {
     const handleSignIn = (event: FormEvent<HTMLIonButtonElement>) => {
         event.preventDefault();
         dispatch(actions.signIn((inputEmail.current as unknown as HTMLIonInputElement).value+"", (inputPass.current as unknown as HTMLIonInputElement).value+""));
-      };
+    };
+
+    const errorManagement = (name: string) => {
+
+        let message = "";
+
+        switch (name) {
+            case "UserNotFoundException":
+                message = "Este usuario no existe.";
+                break;
+
+            case "NotAuthorizedException":
+                message = "Usuario y/o contraseña no valida.";
+                break;
+            
+        }
+ 
+        return message;
+    }
+
+    const disableButton = (inputEmail: string, inputPass: string) => {
+
+        let disable = true;
+
+        if (inputEmail !== ""){
+            if(inputPass !== ""){
+                disable = false;
+            }
+        }
+
+        return disable;
+    }
 
     return (
         <IonContent className="ion-padding" color="light">
@@ -35,8 +66,15 @@ const SignIn: React.FC<props> = props => {
                             <IonCardTitle className="ion-text-center"><IonImg className="logoimage" src={logo} alt="mediacloud"/></IonCardTitle>
                         </IonCardHeader>
 
+                        
+                        {console.log(inputEmail)};
+
                         <IonCardContent className="ion-justify-content-center max-height ion-align-items-center">
-                            {(messageError) ? <span>{messageError.message}</span> : null}
+                            {(messageError) ? 
+                            <IonItem lines="none">
+                                <IonLabel>{errorManagement(messageError.name)}</IonLabel>
+                            </IonItem> 
+                            : null}
                             <IonItem lines="inset">
                                 <IonLabel position="floating">Email</IonLabel>
                                 <IonInput ref={inputEmail} type="email" name="email" value={props.userData.email} onIonChange={props.handleFormInput}></IonInput>    
@@ -46,7 +84,7 @@ const SignIn: React.FC<props> = props => {
                                 <IonInput ref={inputPass} type="password" name="password" value={props.userData.password}  onIonChange={props.handleFormInput}></IonInput>    
                             </IonItem>
 
-                            <IonButton expand="block" onClick={handleSignIn}>Iniciar Sesión</IonButton>
+                            <IonButton expand="block" onClick={handleSignIn} disabled={disableButton(props.userData.email, props.userData.password)}>Iniciar Sesión</IonButton>
                             <IonButton expand="block" color="secondary"  onClick={() => {dispatch(actions.switchComponent("signup"))}}>Registrarse</IonButton>
                             <IonItem className="ion-margin-top" lines="none">
                                 <IonText className="ion-text-center">¿Has olvidado tu contraseña?<br/> <IonButton onClick={() => {dispatch(actions.switchComponent("forgotpass"))}} className="forgot-password-link">Pulsa aquí.</IonButton></IonText>
