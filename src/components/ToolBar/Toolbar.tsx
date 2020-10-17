d2f41f7af1a91acf1b00336bac7b0fe788543620
Daniel <daniel.rodajo98@gmail.com>
2020-10-17 13:04:50 +0200

    Agregados iconos en la toolbar

--- src/components/ToolBar/Toolbar.tsx
+++ src/components/ToolBar/Toolbar.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
-import { IonToolbar, IonHeader, IonImg } from '@ionic/react';
+import { IonToolbar, IonHeader, IonImg, IonIcon, IonItem } from '@ionic/react';
 import logo from "../../images/lightlogobg.png";
-import './Toolbar.css'
+import './Toolbar.scss'
+import { notifications, personAdd } from 'ionicons/icons';
 
 
 
 const Toolbar: React.FC= () => {
     return ( 
         <IonHeader>
-            <IonToolbar>
-                <IonImg className="logoimage" src={logo} alt="mediacloud"/>       
+            <IonToolbar>    
+                <IonItem lines="none" className="custom-icons">    
+                    <IonItem lines="none" className="custom-icon" button onClick={(e) => console.log("Notificaciones")}>
+                        <IonIcon  slot="end" icon={notifications}/>
+                    </IonItem> 
+                    <IonItem lines="none" className="custom-icon" button onClick={(e) => console.log("Amigos")}>
+                        <IonIcon  slot="end" icon={personAdd}/>
+                    </IonItem> 
+                </IonItem>     
+                <IonImg className="logoimage" src={logo} alt="mediacloud"/>    
             </IonToolbar>
         </IonHeader>
     );
