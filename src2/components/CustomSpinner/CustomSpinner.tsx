ec50521b0b0dbbf0b12160357b0524e7feaf2ec3
danielrodajo <daniel.rodajo98@gmail.com>
2021-04-05 23:47:53 +0200

    remove src2

--- src2/components/CustomSpinner/CustomSpinner.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { IonSpinner } from '@ionic/react';
-import './CustomSpinner.css'
-
-const CustomSpinner: React.FC = () => (
-    <IonSpinner className="my-spinner" color="tertiary" />
-);
-
-export default CustomSpinner;
